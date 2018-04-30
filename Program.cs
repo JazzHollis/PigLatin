56dc02e36c5d00f3d2e99730a6529b96b2ad4878
Jasmine Hollis <jasmine.hollis@rocketmail.com>
2018-04-30 00:49:07 -0400

    Second commit of Week 1 Capstone

--- Program.cs
+++ Program.cs
@@ -15,17 +15,24 @@ namespace Capstone1_PigLatin
                 Console.WriteLine("Please enter a line to be translated: ");
                 string input = Console.ReadLine();
 
-
+                //Idetify vowels in user input
                 char[] vowels = { 'a', 'e', 'i', 'o', 'u' };
                 int index = input.ToLower().IndexOfAny(vowels);
 
+                //Divide words utilizing the substring
                 string firstVowel = input.Substring(index);
                 string restOfWord = input.Substring(0, index);
+
+                /* If any of the vowels listed are in the first placeholder add 
+                 "way" to the end of input */
 
                 if (index == 0)
                     {
                         Console.WriteLine(input + "way");
                     }
+
+                /*Identify any vowels and make the first one the "0" placeholder add 
+                "ay" to the letters(consonants) that follow */
 
                 else if (index != 0)
                     {
@@ -36,6 +43,7 @@ namespace Capstone1_PigLatin
                 Console.WriteLine("Would you like to translate another line? (Y/N)");
                 string play = Console.ReadLine();
 
+                //Give the user the option to continue
                 if (play.ToLower() != "y")
                     {
                         Console.WriteLine("Thanks for playing!");
